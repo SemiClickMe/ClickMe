9fe2259fabe26cd8ba9e7c31db3f966ea8f3e792
lee@hanmail.net <leejunhan5503@gmail.com>
2021-10-06 17:21:13 +0900

    2021/10/06_17:21 Add : User, PostCat, Report DTO 작성

---
+++ src/org/clickMe/common/model/dto/ReportDTO.java
@@ -0,0 +1,49 @@
+package org.clickMe.common.model.dto;
+
+import java.io.Serializable;
+
+public class ReportDTO implements Serializable{
+
+	private static final long serialVersionUID = -8180867708829928474L;
+
+	private int reportCode;
+	private int postCode;
+	private String reason;
+	
+	public ReportDTO() {
+	}
+	public int getReportCode() {
+		return reportCode;
+	}
+	public void setReportCode(int reportCode) {
+		this.reportCode = reportCode;
+	}
+	public int getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(int postCode) {
+		this.postCode = postCode;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	public ReportDTO(int reportCode, int postCode, String reason) {
+		super();
+		this.reportCode = reportCode;
+		this.postCode = postCode;
+		this.reason = reason;
+	}
+	@Override
+	public String toString() {
+		return "ReportDTO [reportCode=" + reportCode + ", postCode=" + postCode + ", reason=" + reason + "]";
+	}
+	
+}
