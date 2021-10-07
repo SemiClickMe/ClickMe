9770fd01565f097de1c9540db15b61e8ced121f5
leejunhan@hanmail.net <leejunhan5503@gmail.com>
2021-10-07 23:14:47 +0900

    2021/10/07_11:12 ADD : clickMe파일에 member-controllet파일 만들어 MemberLoginSetvlet, MemberLogoutServlet 생성, member-model-dto파일 만들어 MemberDTO, member-service파일 만들어 MemberServlet 생성.

---
+++ src/org/clickMe/member/model/service/MemberService.java
@@ -0,0 +1,12 @@
+package org.clickMe.member.model.service;
+
+import org.clickMe.member.model.dto.MemberDTO;
+
+public class MemberService {
+
+	public MemberDTO loginCheck(MemberDTO requestMember) {
+		
+		return null;
+	}
+
+}
